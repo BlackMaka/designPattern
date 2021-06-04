23ee3f2268003c6f8e8f63c742ad1b8e4ef656e1
suhan <completed0728@gmail.com>
2021-06-04 11:29:39 +0900

    Initial commit

---
+++ src/com/company/design/Main.java
@@ -0,0 +1,19 @@
+package com.company.design;
+
+import com.company.design.singleton.Aclazz;
+import com.company.design.singleton.Bclazz;
+import com.company.design.singleton.SocketClient;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Aclazz aclazz = new Aclazz();
+        Bclazz bclazz = new Bclazz();
+
+        SocketClient aClient = aclazz.getSocketClient();
+        SocketClient bClient = bclazz.getSocketClient();
+
+        System.out.println(aClient.equals(bClient));
+
+    }
+}
---
+++ src/com/company/design/singleton/Aclazz.java
@@ -0,0 +1,12 @@
+package com.company.design.singleton;
+
+public class Aclazz {
+    private SocketClient socketClient;
+    public Aclazz(){
+        this.socketClient = SocketClient.getInstance();
+    }
+
+    public SocketClient getSocketClient(){
+        return this.socketClient;
+    }
+}
